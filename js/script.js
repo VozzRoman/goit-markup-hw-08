79a17a574f1482eb2cf8575ae739ad50861ae144
VozzRoman <looper0500@ukr.net>
2022-08-07 19:08:56 +0300

    correction

--- js/script.js
+++ js/script.js
@@ -1,10 +1,17 @@
 const buttonBuger = document.querySelector(".button-menu");
-const buttonMenuClose = document.querySelector(".button-close__header-page");
+const buttonMenuClose = document.querySelector(".header-page__menu");
 const burgerMenu = document.querySelector(".header-page__menu");
 
 buttonBuger.addEventListener('click', function () {
+	const expanded = buttonBuger.getAttribute("aria-expanded") === "true" || false;
 	burgerMenu.classList.add('active');
+	buttonBuger.setAttribute("aria-expanded", !expanded);
 })
-buttonMenuClose.addEventListener('click', function () {
+buttonMenuClose.addEventListener('click', function (event) {
+	event.stopPropagation();
+	const expanded = buttonBuger.getAttribute("aria-expanded") === "false" || true;
 	burgerMenu.classList.remove('active');
+	
+	buttonBuger.setAttribute("aria-expanded", !expanded);
+	
 })
