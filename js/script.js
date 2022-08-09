f841dba60cd2cd07d1d3f4c7269774b0b40a5c28
VozzRoman <looper0500@ukr.net>
2022-08-09 16:05:58 +0300

    js

--- js/script.js
+++ js/script.js
@@ -1,17 +1,16 @@
 const buttonBuger = document.querySelector(".button-menu");
-const buttonMenuClose = document.querySelector(".header-page__menu");
+const buttonMenuClose = document.querySelector(".button-close-menu");
 const burgerMenu = document.querySelector(".header-page__menu");
 
 buttonBuger.addEventListener('click', function () {
 	const expanded = buttonBuger.getAttribute("aria-expanded") === "true" || false;
-	burgerMenu.classList.add('active');
+	burgerMenu.classList.toggle('active');
 	buttonBuger.setAttribute("aria-expanded", !expanded);
 })
 buttonMenuClose.addEventListener('click', function (event) {
 	event.stopPropagation();
 	const expanded = buttonBuger.getAttribute("aria-expanded") === "false" || true;
 	burgerMenu.classList.remove('active');
-	
 	buttonBuger.setAttribute("aria-expanded", !expanded);
 	
 })
