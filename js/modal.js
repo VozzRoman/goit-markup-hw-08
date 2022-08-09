c83f605664b18f8b20440ec098b6e3c3ffff7eae
VozzRoman <looper0500@ukr.net>
2022-08-09 16:37:24 +0300

    correction

--- js/modal.js
+++ js/modal.js
@@ -2,14 +2,16 @@
   const refs = {
     openModalBtn: document.querySelector("[data-modal-open]"),
     closeModalBtn: document.querySelector("[data-modal-close]"),
-    modal: document.querySelector("[data-modal]"),
+	  modal: document.querySelector("[data-modal]"),
+	
   };
-
+	const bodyLock = document.body;
   refs.openModalBtn.addEventListener("click", toggleModal);
   refs.closeModalBtn.addEventListener("click", toggleModal);
 
   function toggleModal() {
-    refs.modal.classList.toggle("is-hidden");
+	  refs.modal.classList.toggle("is-hidden");
+	  bodyLock.classList.toggle("lock");
   }
 })();
 
--- js/script.js
+++ js/script.js
@@ -1,16 +1,19 @@
 const buttonBuger = document.querySelector(".button-menu");
 const buttonMenuClose = document.querySelector(".button-close-menu");
 const burgerMenu = document.querySelector(".header-page__menu");
+const bodyLock = document.body;
 
 buttonBuger.addEventListener('click', function () {
 	const expanded = buttonBuger.getAttribute("aria-expanded") === "true" || false;
-	burgerMenu.classList.toggle('active');
+	burgerMenu.classList.add('active');
+	bodyLock.classList.add('lock');
 	buttonBuger.setAttribute("aria-expanded", !expanded);
 })
 buttonMenuClose.addEventListener('click', function (event) {
 	event.stopPropagation();
 	const expanded = buttonBuger.getAttribute("aria-expanded") === "false" || true;
 	burgerMenu.classList.remove('active');
+	bodyLock.classList.remove('lock');
 	buttonBuger.setAttribute("aria-expanded", !expanded);
 	
 })
